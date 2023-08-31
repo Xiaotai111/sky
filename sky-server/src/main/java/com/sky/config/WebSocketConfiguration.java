4a2bd5556399a5d373f2e3ce54d2a22c54a70977
小台 <2703093894@qq.com>
2023-08-31 16:46:21 +0800

    来单提醒和用户催单功能

---
+++ sky-server/src/main/java/com/sky/config/WebSocketConfiguration.java
@@ -0,0 +1,19 @@
+package com.sky.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * WebSocket配置类，用于注册WebSocket的Bean
+ * 将ServerEndpointExporter声明为一个Bean，Spring会自动扫描和注册带有@ServerEndpoint注解的WebSocket端点。
+ */
+@Configuration
+public class WebSocketConfiguration {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
